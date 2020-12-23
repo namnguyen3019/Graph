93608b2ae5b096b85658e2595e51715225d4a229
Nam Nguyen <nam.nguy3019@gmail.com>
2020-12-23 10:06:04 -0600

    using heapq to store unvisited nodes in graph

--- Dijkstra/dijkstra-early-stop.py
+++ Dijkstra/dijkstra-early-stop.py
@@ -2,6 +2,7 @@
     This version of Dijkstra will stop when reach the end node.
     Do not need to travel every node in the graph
 '''
+import heapq
 graph = {
     'A': {'B': 50, 'C': 45, 'D': 10},
     'B': {'C': 10, 'D': 15},
@@ -13,41 +14,42 @@ graph = {
 
 
 def dijkstra(graph, start, end):
-    # unvisitedNodes store all unvisited nodes in the graph
-    unvisitedNodes = graph
+    # unvisitedNodes store all unvisited nodes and its weight (or cost) in the graph
+    unvisitedNodes = []
+    # Go to "start" location to itself cost 0. Push it in the priority queue
+    heapq.heappush(unvisitedNodes, (0, start))
 
     # records of shorest path from start to a node
     shortest_path = {}
     track_precessor = {}
     # initialize the shorest_path with value to everynode is infinity
-    for node in unvisitedNodes:
+    for node in graph:
         shortest_path[node] = float('inf')
     # exception for start node: distance from start node to itself is 0
     shortest_path[start] = 0
 
     # loop through all unvisted nodes
     while unvisitedNodes:
-        # find the next min node of current node
-        min_node = None
-        for node in unvisitedNodes:
-            if min_node is None:
-                min_node = node
-            elif shortest_path[node] < shortest_path[min_node]:
-                min_node = node
+        # find the next node that is nearest the current node
+        # Also pop the node from the priority queue
+        (min_cost, selected_node) = heapq.heappop(unvisitedNodes)
 
         # Break the loop when reach the end_node => BREAK EARLY
-        if min_node == end:
+        if selected_node == end:
             print('Stop early')
             break
 
         # loop through every options of the next node just found
-        path_options = graph[min_node].items()
+        path_options = graph[selected_node].items()
         for child_node, weight in path_options:
-            if weight + shortest_path[min_node] < shortest_path[child_node]:
-                shortest_path[child_node] = weight + shortest_path[min_node]
-                track_precessor[child_node] = min_node
+            # Push node in the priority queue
+            heapq.heappush(unvisitedNodes, (weight, child_node))
 
-        unvisitedNodes.pop(min_node)
+            if weight + min_cost < shortest_path[child_node]:
+                shortest_path[child_node] = weight + \
+                    shortest_path[selected_node]
+                track_precessor[child_node] = selected_node
+        heapq.heappop(unvisitedNodes)
     # back tracking from to end to start
     current_node = end
     optimal_path = []
@@ -65,4 +67,4 @@ def dijkstra(graph, start, end):
         print('Total cost: ' + str(shortest_path[end]))
 
 
-dijkstra(graph, 'A', 'E')
+dijkstra(graph, 'B', 'C')
